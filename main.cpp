c88e1032293c85db928a3be54ae8adf736c9680f
lsgrge <lg76420@gmail.com>
2019-03-15 15:17:37 +0100

    bfs

--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 
 int main()
 {
+    //std::string id;
     graphe g{"graphe_2.txt"};
+
     g.afficher();
+
+    //std::cout << "Entrer le sommet pour parcourir le graphe : ";
+    //std::cin >> id;
+
     g.afficherBFS("1");
     g.afficherDFS("1");
-    int ncc=g.rechercher_afficherToutesCC();
+
+    int ncc = g.rechercher_afficherToutesCC();
     std::cout<<"le graphe est-il eulerien ? a coder ..."<<std::endl<<std::endl;
+
     return 0;
 }
--- sommet.cpp
+++ sommet.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <queue>
 #include <stack>
-#include<unordered_map>
-#include<unordered_set>
+#include <unordered_map>
+#include <unordered_set>
 #include "sommet.h"
 
 Sommet::Sommet(std::string id,double x,double y):m_id{id},m_x{x},m_y{y}
@@ -21,16 +21,42 @@ void Sommet::afficherVoisins() const{
     }
 }
 
-std::unordered_map<std::string,std::string> Sommet::parcoursBFS() const{
+std::unordered_map<std::string,std::string> Sommet::parcoursBFS() const
+{
     std::unordered_map<std::string,std::string> l_pred;
-    std::cout<<"BFS a completer"<<std::endl;
+    //std::cout<<"BFS a completer"<<std::endl;
+    std::queue<const Sommet*> file;
+    const Sommet* s;
+    file.push(this);
+
+    while(!file.empty())
+    {
+      s = file.front(); //on récupère le premier élément de la file
+      file.pop(); //on vide la file du premier élément
+
+      for(size_t i = 0; i < s->m_voisins.size(); i++)
+      {
+
+        if((l_pred.find(s->m_voisins[i]->m_id)) == (l_pred.end()) && (s->m_voisins[i] != this))
+        {
+          file.push(s->m_voisins[i]);
+          l_pred.insert(s->m_voisins[i]->m_id, s->m_id);
+        }
+
+      }
+    }
+
     return l_pred;
 }
+
+
 std::unordered_map<std::string,std::string> Sommet::parcoursDFS() const{
     std::unordered_map<std::string,std::string> l_pred;
     std::cout<<"DFS a completer"<<std::endl;
     return l_pred;
 }
+
+
 std::unordered_set<std::string> Sommet::rechercherCC() const{
     std::unordered_set<std::string> cc;
     std::cout<<"rechercherCC a coder"<<std::endl;
